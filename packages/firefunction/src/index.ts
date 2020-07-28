2552012dd8dd954d60d0e28013dfdc51ecf9eb5a
baekkyoungjung <baekkyoungjung@gmail.com>
2020-07-28 20:00:20 +0900

    feat(function): 모델 monorepo 에서 꺼내보기

--- packages/firefunction/src/index.ts
+++ packages/firefunction/src/index.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions';
+import { user } from '@playpet/models';
 
+const uuu:user = {
+  haha: '123',
+};
+console.log('uuu', uuu);
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
