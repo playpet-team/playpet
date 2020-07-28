54b898e160ed3cf428972c89af59095bd3f545e9
baekkyoungjung <baekkyoungjung@gmail.com>
2020-07-28 20:00:03 +0900

    feat(models): 모델 package 추가

---
+++ packages/models/dist/index.js
@@ -0,0 +1,16 @@
+(() => {
+  var __commonJS = (callback, module) => () => {
+    if (!module) {
+      module = {exports: {}};
+      callback(module.exports, module);
+    }
+    return module.exports;
+  };
+
+  // index.ts
+  var require_models = __commonJS(() => {
+  });
+
+  // src/user.ts
+  require_models();
+})();
