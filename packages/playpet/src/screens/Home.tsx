462e155c8fd0ce932b861e53de751bbf3554dbca
baekkyoungjung <baekkyoungjung@gmail.com>
2020-07-29 17:54:44 +0900

    feat(client): update 로직 설정, release-channel 설정

--- packages/playpet/src/screens/Home.tsx
+++ packages/playpet/src/screens/Home.tsx
@@ -4,12 +4,14 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from '@emotion/native';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/rootReducers';
+import * as Updates from 'expo-updates';
 
 const HomeBlock = styled.View`
     display: flex;
 `;
 
 export default function Home() {
+    console.log('Updates-------', Updates);
     const { profileImage } = useSelector((state: RootState) => state.auth);
     return (
         <SafeAreaView>
