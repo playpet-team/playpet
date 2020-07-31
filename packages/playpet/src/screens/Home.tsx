faec821e9ad1f039073df7a5942f6230eaa082ff
baekkyoungjung <baekkyoungjung@gmail.com>
2020-07-31 22:43:06 +0900

    feat(client): Home 단계에서 auth 체크하고 subscribe 시킴 -> unmount 시 구독해지

--- packages/playpet/src/screens/Home.tsx
+++ packages/playpet/src/screens/Home.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from '@emotion/native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../store/rootReducers';
-import * as Updates from 'expo-updates';
+import auth from '@react-native-firebase/auth';
+import { authActions } from '../store/authReducer';
 
 const HomeBlock = styled.View`
     display: flex;
 `;
 
 export default function Home() {
-    console.log('Updates-------', Updates);
-    const { profileImage } = useSelector((state: RootState) => state.auth);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+    function onAuthStateChanged(user: any) {
+        if (user) {
+            dispatch(authActions.signIn());
+        } else {
+            dispatch(authActions.signOut());
+        }
+    }
+
+    // const { profileImage, isLogged } = useSelector((state: RootState) => state.auth);
     return (
         <SafeAreaView>
             <HomeBlock>
