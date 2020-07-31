8b638c6293f69337e3913e1b97f268e0d56aed7f
baekkyoungjung <baekkyoungjung@gmail.com>
2020-07-31 22:44:14 +0900

    feat(client): reducer 일단 로그인만

--- packages/playpet/src/store/authReducer.tsx
+++ packages/playpet/src/store/authReducer.tsx
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { User } from '@playpet/firefunction';
 
-export interface Member {
-    profileImage: string;
-};
-
-const initialState: Member = {
+interface AuthSettings extends User {
+    isLogged: boolean;
+}
+const initialState: AuthSettings = {
+    isLogged: false,
+    uid: '',
     profileImage: '',
 };
 
@@ -12,7 +14,15 @@ const slice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
+        signIn(state) {
+            state.isLogged = true;
+        },
+        signOut(state) {
+            state.isLogged = false;
+        },
     },
 });
 
+// console.log('slice', slice);
+export const authActions = slice.actions;
 export default slice.reducer;
