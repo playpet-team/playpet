797b2e4588493f26f3e20ee27df8ec02a6ea5334
baekkyoungjung <baekkyoungjung@gmail.com>
2020-07-31 22:50:45 +0900

    feat(model): user model properties 추가

--- packages/firefunction/src/models/src/user.ts
+++ packages/firefunction/src/models/src/user.ts
@@ -1,3 +1,12 @@
 export interface User {
     uid: string;
+    isLeave: boolean;
+    leaveAt: string;
+    username: string;
+    gender: string;
+    birthDate: string;
+    phoneNumber: string;
+    photo: string,
+    createdAt: string;
+    updatedAt: string;
 };
