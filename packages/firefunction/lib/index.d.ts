dfd56888a298e6c05fb653d827c6115af2958340
baekkyoungjung <baekkyoungjung@gmail.com>
2020-08-02 00:33:31 +0900

    feat(firefunctions): 설정파일 조금 손봄

--- packages/firefunction/lib/index.d.ts
+++ packages/firefunction/lib/index.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="express" />
 import * as functions from 'firebase-functions';
-export declare const helloWorld: functions.HttpsFunction;
-export declare const createUser: functions.TriggerAnnotated & ((req: functions.Request<import("express-serve-static-core").ParamsDictionary>, resp: functions.Response<any>) => void | Promise<void>) & functions.Runnable<any>;
+export declare const createUserCollection: functions.TriggerAnnotated & ((req: functions.Request<import("express-serve-static-core").ParamsDictionary>, resp: functions.Response<any>) => void | Promise<void>) & functions.Runnable<any>;
