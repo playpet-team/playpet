c723da4c632ed86e38b052cb002f45339ee9df0b
baekkyoungjung <baekkyoungjung@gmail.com>
2020-08-02 00:33:48 +0900

    feat(firefunctions): user document 생성 function 생성

--- packages/firefunction/src/index.ts
+++ packages/firefunction/src/index.ts
@@ -1,25 +1,46 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+const serviceAccount = require('./playpet-5b432-261d6cdeaba9.json');
 
-// import { User } from './models';
-
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// admin gcp 권한 json -> 보관에 주의해야함
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
 
-interface userParams {
+const getCurrentTime = () => admin.firestore.FieldValue.serverTimestamp();
+
+interface createUserParams {
     uid: string;
-};
-export const userDetail = functions.https.onCall(async ({ uid }: userParams) => {
+    method: string;
+}
+
+export const createUserCollection = functions.https.onCall(async ({ uid, method }: createUserParams) => {
     try {
-        return (await admin.firestore()
-            .collection('users')
-            .doc(uid)
-            .get()).data();
+        const {
+            email,
+            displayName = '',
+            phoneNumber = '',
+            photoURL = '',
+        } = await admin.auth().getUser(uid);
+
+        await admin.firestore().collection('users').doc(uid).set({
+            uid,
+            method,
+            isLeave: false,
+            username: displayName,
+            email,
+            phoneNumber,
+            photo: photoURL,
+            leaveAt: '',
+            gender: '',
+            birthDate: '',
+            lastLogin: getCurrentTime(),
+            createdAt: getCurrentTime(),
+            updatedAt: getCurrentTime(),
+        });
+        return { result: 'signUp', status: 'SUCCESS' };
     } catch (error) {
-        console.error('userDetail: ', error);
-        return null;
+        console.error('createUserCollection--error--', error);
+        return { result: 'error', status: 'SUCCESS' };
     }
 });
