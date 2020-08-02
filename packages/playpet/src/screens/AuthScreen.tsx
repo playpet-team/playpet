929872725d2b4d8b04bb20bf38e6fdf279a94865
baekkyoungjung <baekkyoungjung@gmail.com>
2020-08-02 23:41:49 +0900

    feat(client): profile 컴포넌트 분리

--- packages/playpet/src/screens/AuthScreen.tsx
+++ packages/playpet/src/screens/AuthScreen.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
 import styled from '@emotion/native';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/rootReducers';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
-import { signOut } from '../utils';
-import { signEnum } from '../models';
 import SignUpAgreeTermsModal from './AuthScreen/SignUpAgreeTermsModal';
 import SocialSignIn from './AuthScreen/SocialSignIn';
 import ProfileSection from '../components/ProfileSection';
@@ -17,16 +13,9 @@ export default function AuthScreen() {
 
     return (
         <>
-            <ProfileSection />
-            <Text>로그인여부: {user.isLogged ? 'yes' : 'no'}</Text>
-            <Text>네임: {user.username}</Text>
 
             {user.isLogged ?
-                <TouchableOpacity
-                    onPress={() => signOut(signEnum.GOOGLE)}
-                >
-                    <Text>로그아웃</Text>
-                </TouchableOpacity>
+                <ProfileSection />
                 :
                 <SocialSignIn setModalVisible={setModalVisible} />
             }
