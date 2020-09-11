f05cbadf0941fa58eb5ae3a9e744c58740028d1b
baekkyoungjung <baekkyoungjung@gmail.com>
2020-09-11 14:12:22 +0900

    feat: sentry release version 체크

--- src/utils/system/sentry.ts
+++ src/utils/system/sentry.ts
@@ -1,9 +1,11 @@
 import * as Sentry from "@sentry/react-native";
+import * as Updates from 'expo-updates';
 
 export const sentryInit = () => {
     Sentry.init({
         dsn: 'https://8e273cf95abf4bed98c3a636731c14b2@o444254.ingest.sentry.io/5418939',
         environment: __DEV__ ? 'development' : 'production',
         debug: __DEV__,
+        release: `v.${Updates.manifest.version}` || 'dev',
     });
 }
